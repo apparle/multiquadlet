f16b52eac08047809907d6bf9f29b492133265a2
Apoorv Parle <19315187+apparle@users.noreply.github.com>
2025-08-21 00:55:40 -0700

    Add support for more file types and add correct Unit SourcePath

--- multiquadlet_gen.py
+++ multiquadlet_gen.py
@@ -15,24 +15,104 @@ def log_with_level(level, message):
     """Logs a message with a specified kernel log level."""
     print(f"multiquadlet_gen[{level}]: {message}")
 
-def process_target_install_section(normal_dir, target_unit_name):
-    """Parses a .target file and creates symlinks based on its [Install] section."""
-    intermediate_target_file = os.path.join(normal_dir, target_unit_name)
+def get_quadlet_service_filename(fname):
+    just_name, extension = os.path.splitext(fname)
+    if extension == '.container':
+        return f"{just_name}.service"
+    elif extension in ['.pod', '.kube', '.network', '.volume', '.image', '.build']:
+        file_type = extension[1:]
+        return f"{just_name}-{file_type}.service"
+    elif extension in ['.target', '.socket', '.service', '.timer']:
+        return fname
+    else:
+        log_with_level(3, f"Error: Unknown file type {fname} while trying to figure out service name. Skipping.")
+        return None
 
-    if not os.path.isfile(intermediate_target_file):
-        log_with_level(3, f"Error: Intermediate target file not found: {intermediate_target_file}. Skipping.")
+def update_source_path(f, source_file):
+    if not os.path.isfile(f):
+        log_with_level(3, f"Error: Intermediate quadlet generated file not found: {f}. Skipping.")
         return True
 
     config = SystemdUnitParser()
     try:
-        config.read(intermediate_target_file)
+        config.read(f)
     except Exception as e:
-        print(f"Error: Failed to parse '{intermediate_target_file}': {e}", file=sys.stderr)
-        log_with_level(3, f"Error: failed to parse file {intermediate_target_file}")
+        print(f"Error: Failed to parse '{f}': {e}", file=sys.stderr)
+        log_with_level(3, f"Error: failed to parse file {f}")
+        return True
+
+    source_path_present = config.get('Unit', 'SourcePath', fallback=None)
+
+    infile = os.path.basename(f)
+    content = []
+    
+    try:
+        with open(f, 'r') as fr:
+            lines = fr.read().splitlines()
+        for line in lines:
+            if source_path_present:
+                match = re.match(r"^\s*SourcePath=.*", line)
+                if match:
+                    content.append(f"SourcePath={source_file}")
+                    log_with_level(6, f"Updated 'SourcePath={source_file}' in {f}")
+                else:
+                    content.append(line)
+            else:
+                content.append(line)
+                match = re.match(r"^\s*\[Unit\]\s*", line)
+                if match:
+                    content.append(f"SourcePath={source_file}")
+                    log_with_level(6, f"Inserted 'SourcePath={source_file}' in '{f}'")
+        with open(f, 'w') as fw:
+            fw.write('\n'.join(content))
+            fw.write('\n')
+    except IOError as e:
+        log_with_level(3, f"Error: Cannot process input file '{f}', skipping: {e}")
+        return True 
+
+
+def update_source_path_old(f, source_file):
+    if not os.path.isfile(f):
+        log_with_level(3, f"Error: Intermediate quadlet generated file not found: {f}. Skipping.")
+        return True
+
+    config = SystemdUnitParser()
+    try:
+        config.read(f)
+    except Exception as e:
+        print(f"Error: Failed to parse '{f}': {e}", file=sys.stderr)
+        log_with_level(3, f"Error: failed to parse file {f}")
+        return False
+
+    config.set('Unit', 'SourcePath', source_file)
+    log_with_level(6, f"Updated 'SourcePath' to '{source_file}' in '{f}'")
+
+    try:
+        with open(f, 'w') as configfile:
+            config.write(configfile)
+    except IOError as e:
+        log_with_level(3, f"Error writing to file {f}: {e}")
+        return False
+    
+
+def process_unit_install_section(normal_dir, unit_name):
+    """Parses a systemd file and creates symlinks based on its [Install] section."""
+    intermediate_unit_file = os.path.join(normal_dir, unit_name)
+
+    if not os.path.isfile(intermediate_unit_file):
+        log_with_level(3, f"Error: Intermediate unit file not found: {intermediate_unit_file}. Skipping.")
+        return True
+
+    config = SystemdUnitParser()
+    try:
+        config.read(intermediate_unit_file)
+    except Exception as e:
+        print(f"Error: Failed to parse '{intermediate_unit_file}': {e}", file=sys.stderr)
+        log_with_level(3, f"Error: failed to parse file {intermediate_unit_file}")
         return False
 
     if 'Install' not in config:
-        log_with_level(6, f"No [Install] section found in {target_unit_name}. Skipping.")
+        log_with_level(6, f"No [Install] section found in {unit_name}. Skipping.")
         return True
 
     dependencies = {
@@ -50,22 +130,23 @@ def process_target_install_section(normal_dir, target_unit_name):
         else:
             targets = sum([v.split() for v in dep_list],targets)
 
-        log_with_level(6, f"Installing {target_unit_name} as type={dep_type} for target units = {targets}")
+        log_with_level(6, f"Installing {unit_name} as type={dep_type} for target units = {targets}")
         for target in targets:
             symlink_dir = os.path.join(normal_dir, f"{target}.{dep_type}")
-            symlink_path = os.path.join(symlink_dir, target_unit_name)
+            symlink_path = os.path.join(symlink_dir, unit_name)
 
             try:
                 os.makedirs(symlink_dir, exist_ok=True)
-                os.symlink(os.path.join(normal_dir, target_unit_name), symlink_path)
-                log_with_level(6, f"Created symlink: {symlink_path} -> {os.path.join(normal_dir, target_unit_name)} (type: {dep_type})")
+                relative_target_path = os.path.relpath(os.path.join(normal_dir, unit_name), symlink_dir)
+                os.symlink(relative_target_path, symlink_path)
+                log_with_level(6, f"Created symlink: {symlink_path} -> {relative_target_path} (type: {dep_type})")
             except FileExistsError:
                 log_with_level(7, f"Symlink already exists at {symlink_path}.")
             except OSError as e:
                 log_with_level(3, f"Error creating symlink {symlink_path}: {e}")
                 return False
     
-    log_with_level(6, f"Finished processing Install section for {target_unit_name}")
+    log_with_level(6, f"Finished processing Install section for {unit_name}")
     return True
 
 def main():
@@ -80,10 +161,6 @@ def main():
     else:
         input_dir = os.path.abspath("/etc/containers/multiquadlet")
 
-    interim_tmp_dir = tempfile.TemporaryDirectory(prefix='multiquadlet_gen_')
-    interimdir = interim_tmp_dir.name
-    log_with_level(6, f"Using temporary directory for intermediate quadet files: {interimdir}")
-
     if not len(sys.argv) > 1:
         log_with_level(3, f"Error: Must be run as: {os.path.basename(__file__)} gendir [gendir-early] [gendir-late]")
     gendir = sys.argv[1]
@@ -92,29 +169,35 @@ def main():
         log_with_level(4, f"Warning: Input directory '{input_dir}' does not exist. Skipping multiquadlet processing.")
         sys.exit(0)
 
+    interim_top_dir = tempfile.TemporaryDirectory(prefix='multiquadlet_gen_')
+    interimdir = os.path.join(interim_top_dir.name, "multiquadlet_interim")
+    os.makedirs(interimdir, exist_ok=True)
+    log_with_level(6, f"Using temporary directory for intermediate quadet files: {interimdir}")
+
+    source_file_map = {}
 
     # Copy files
     try:
         files_to_copy = [f for f in os.listdir(input_dir) if re.match(r".*\.(container|network|volume|pod)$", f)]
         for f in files_to_copy:
-            shutil.copy(os.path.join(input_dir, f), interimdir)
+            infile = os.path.join(input_dir, f)
+            shutil.copy(infile, interimdir)
+            source_file_map[get_quadlet_service_filename(f)] = infile
     except FileNotFoundError:
-        pass # Directory might not exist, handled above
+        pass
+
 
     # Process multiquadlet files
     for infile in [f for f in os.listdir(input_dir) if f.endswith('.multiquadlet')]:
         log_with_level(6, f"Processing input file: {infile}")
         input_path = os.path.join(input_dir, infile)
         files_generated_content = {}
-        
         try:
             with open(input_path, 'r') as f:
                 lines = f.read().splitlines()
-                
             current_content = []
             current_fname = None
             skip_file = False
-
             for line in lines:
                 match = re.match(r"^---\ (.+)\ ---$", line)
                 if match:
@@ -136,28 +219,32 @@ def main():
                 log_with_level(6, f"Generated files from {infile}:")
                 for fname in files_generated_content.keys():
                     output_path = os.path.join(interimdir, fname)
-                    with open(output_path, 'w') as out_f:
-                        out_f.write(f"# Automatically generated by {os.path.basename(__file__)} from {os.path.basename(infile)}\n")
-                        out_f.write('\n'.join(files_generated_content[fname]))
-                        out_f.write('\n')
+                    with open(output_path, 'w') as fw:
+                        fw.write(f"# Automatically generated by {os.path.basename(__file__)} from {os.path.basename(infile)}\n")
+                        fw.write('\n'.join(files_generated_content[fname]))
+                        fw.write('\n')
                     log_with_level(6, f"  {output_path}")
+                    source_file_map[get_quadlet_service_filename(fname)] = input_path
             else:
                 log_with_level(6, f"No files generated from {infile}.")
         except IOError as e:
             log_with_level(3, f"Error: Cannot read input file '{input_path}', skipping: {e}")
             continue
 
+    
     # Run podman generator
+    interimdir_gen = os.path.join(interim_top_dir.name, "generator")
+    interimdir_gen_early = os.path.join(interim_top_dir.name, "generator.early")
+    interimdir_gen_late = os.path.join(interim_top_dir.name, "generator.late")
     try:
         podman_generator_path = f"/usr/lib/systemd/{systemd_scope}-generators/podman-{systemd_scope}-generator"
-        result = subprocess.run([podman_generator_path, *sys.argv[1:]], env={**os.environ, 'QUADLET_UNIT_DIRS': interimdir}, check=True, capture_output=True, text=True)
+        result = subprocess.run([podman_generator_path, interimdir_gen, interimdir_gen_early, interimdir_gen_late], env={**os.environ, 'QUADLET_UNIT_DIRS': interimdir}, check=True, capture_output=True, text=True)
         log_with_level(6, f"Output of podman quadlet generator: \nSTDOUT: \n{result.stdout} \nSTDERR: \n{result.stderr}")
         if result.returncode != 0:
             log_with_level(3, f"Error: podman generator failed with exit code {result.returncode}.")
             sys.exit(result.returncode)
         else:
             log_with_level(6, f"Podman generator completed successfuly.")
-
     except FileNotFoundError:
         log_with_level(3, f"Error: podman generator not found at '{podman_generator_path}'.")
         sys.exit(1)
@@ -165,14 +252,20 @@ def main():
         log_with_level(3, f"Error: unexpected error while running podman generator: {e}.")
         sys.exit(1)
 
+    # Fix up the SourcePath in the generated files
+    for fname in [f for f in os.listdir(interimdir_gen) if os.path.isfile(os.path.join(interimdir_gen, f))]:
+        update_source_path(os.path.join(interimdir_gen, fname), source_file_map[fname])
+    shutil.copytree(interimdir_gen, gendir, symlinks=True, dirs_exist_ok=True)
+
     # Process and copy generated target files
     for fname in os.listdir(interimdir):
-        if fname.endswith(".target"):
+        if fname.endswith(('.target', '.socket', '.service', '.timer')):
             dest_file = os.path.join(gendir, fname)
             if not os.path.exists(dest_file):
+                update_source_path(os.path.join(interimdir, fname), source_file_map[fname])
                 log_with_level(6, f"Copying '{fname}' to '{gendir}'...")
                 shutil.copy(os.path.join(interimdir, fname), dest_file)
-                process_target_install_section(gendir, fname)
+                process_unit_install_section(gendir, fname)
             else:
                 log_with_level(3, f"Error: {dest_file} already exists, skipping.")
 
